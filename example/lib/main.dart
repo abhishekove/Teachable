994519faed30de3f0b6a9a8f39fbbd7d900352b5
abhishekove <abhishekove@gmail.com>
2021-07-02 23:52:55 +0530

    fixed dart example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -60,6 +60,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  String pose = "";
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -69,45 +70,47 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Expanded(
+        appBar: AppBar(title: Text("Pose Classifier")),
+        body: Stack(
+          children: [
+            Container(
+                child: Column(children: <Widget>[
+              Expanded(
                 child: Container(
-              child: Teachable(
-                path: "pose/index.html",
-                drawPose: false,
-                results: (res) {
-                  var resp = jsonDecode(res);
-                  print("The values are $resp");
-                },
+                  child: Teachable(
+                    path: "help/index.html",
+                    drawPose: false,
+                    results: (res) {
+                      var resp = jsonDecode(res);
+                      print("The values are $resp");
+                    },
+                  ),
+                ),
               ),
-            )),
+            ])),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                  width: double.infinity,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    color: Colors.black.withOpacity(0.5),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Text(
+                            pose,
+                          ),
+                        ],
+                      ),
+                    ],
+                  )),
+            )
           ],
-        ),
-      ),
-    );
+        ));
   }
 }
